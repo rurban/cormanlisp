6b1d1587071e3249caca79d97fb07c10a121baf5
Reini Urban <reini.urban@gmail.com>
2026-06-20 19:00:00 +0200

    build: add cormanlisp_img target for Lisp image
    Adds custom target 'cormanlisp_img' that invokes makeimg.sh
    to build CormanLisp.img from Sys/*.lisp sources. The target
    is non-ALL (build on demand via 'make cormanlisp_img').
    
    Image build currently fails due to compiler bootstrap bug
    (header type corruption in macro expansion).

--- makeimg.sh
+++ makeimg.sh
@@ -2,7 +2,7 @@
 # Linux equivalent of makeimg.bat
 # Builds the Corman Lisp image file by loading and compiling all .lisp sources.
 
-set -e
+set -e  # exit on error (ignore for bootstrap)
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 CLCONSOLE="${CLCONSOLE:-$SCRIPT_DIR/build/clconsole}"
 
